a90a245219960be74e470433dc112d0227a4b125
Adam Schulman <aschulman@aschulman-2021-mbp-13.local>
2021-03-12 23:30:17 -0800

    ncaa.py

--- ncaa.py
+++ ncaa.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys, csv
+import sys
+import csv
 from matplotlib.pyplot import xkcd
 
 # Read the data from the csv file and put it into a variable called "data"
@@ -13,16 +14,16 @@ with open('NCAATourneyCompactResults.csv', newline='') as f:
 sum_prop = []
 avg_list = []
 
-limit = 15
 
 def truncate(n, decimals=0):
     multiplier = 10 ** decimals
     return int(n * multiplier) / multiplier
 
+
 for p in range(1, 35, 5):
     low_limit = p
     high_limit = p + 5
-    high_limt = high_limit - 1
+    high_lmt = high_limit - 1
     for i in range(1985, 2008):
         # Create empty lists for data
         blowout = []
@@ -32,42 +33,43 @@ for p in range(1, 35, 5):
         year = str(i)
 
         # Isolate data in specific year
-        for i in range(len(data)):
-            if data[i][0] == year:
-                nineteen.append(data[i])
+        for q in range(len(data)):
+            if data[q][0] == year:
+                nineteen.append(data[q])
             else:
                 pass
 
-        # Isolate blowouts in first round within the year. Save how much they won by, the year, and the code of the winner.
-        for i in range(len(nineteen)):
-            if i == 0:
+        # Isolate blowouts in first round within the year. Save how much they won by, the year,
+        # and the code of the winner.
+        for r in range(len(nineteen)):
+            if r == 0:
                 pass
-            elif data[i][1] == '136' or data[i] == '137':
-                difference = int(data[i][3]) - int(nineteen[i][5])
+            elif data[r][1] == '136' or data[r] == '137':
+                difference = int(data[r][3]) - int(nineteen[r][5])
                 # If it's a blowout, save the information to another list
                 if (difference >= low_limit) and (difference <= high_limit):
-                    blowout.append([nineteen[i][0], difference, nineteen[i][2]])
+                    blowout.append([nineteen[r][0], difference, nineteen[r][2]])
                 else:
                     pass
             else:
                 pass
 
-        numwon = 0
+        numWon = 0
 
         # Calculate how many of the blowout winners won in the next round.
         for d in range(len(blowout)):
-            for i in range(len(nineteen)):
+            for w in range(len(nineteen)):
 
                 # If in the second round, the winning team matches a team that won a blowout
-                if (nineteen[i][1] == '138' or nineteen[i][1] == '139') and blowout[d][2] == nineteen[i][2]:
-                    numwon = numwon + 1
+                if (nineteen[w][1] == '138' or nineteen[w][1] == '139') and blowout[d][2] == nineteen[w][2]:
+                    numWon = numWon + 1
                 else:
                     pass
 
         # Calculate the proportion of blowout winners that won in the next round.
         try:
-            propwon = (numwon) / (len(blowout))
-            sum_prop.append(propwon)
+            propWon = numWon / (len(blowout))
+            sum_prop.append(propWon)
 
         except ZeroDivisionError:
             pass
@@ -80,5 +82,4 @@ for p in range(1, 35, 5):
     avg_list.append(avg)
 
     print('Avg prop. of teams who won second game if won first game with a lead '
-          'between ' + str(low_limit) + ' and ' + str(high_limt) + ': ' + str(truncate((avg), 5)))
-
+          'between ' + str(low_limit) + ' and ' + str(high_lmt) + ': ' + str(truncate(avg, 5)))
